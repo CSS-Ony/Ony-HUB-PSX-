96344fc5bdb5b3fc03d263b9c0fee03c51508b2d
Witch_Oni <125810135+Onizerty@users.noreply.github.com>
2023-02-27 00:06:31 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,21 +2,38 @@ local Start=os.clock()
 
 local UI=loadstring(game:HttpGetAsync'https://raw.githubusercontent.com/max9597/Scripts/lt2/uisD')():Initiate();
 
+local HumanoidSectionTab4=demo4:CreateSection()
+local PlSettings=HumanoidSectionTab4:CreateSubSection('Player Settings');
+local Rewards=HumanoidSectionTab4:CreateSubSection('Player Rewards');
+
 local Trades=UI:CreateTab(5181994100,'TradeScam');
 local TradeSC=Trades:CreateSection()
 local TradeScam=TradeSC:CreateSubSection('TradeScam');
 
-TradeScam:CreateButton('Trade Scam Off/On',function()
-    if LP.PlayerGui.Trading.Frame.Trade.Client.Pets.Visible then
-        LP.PlayerGui.Trading.Frame.Trade.Client.Pets.Visible = false
-    else
-        LP.PlayerGui.Trading.Frame.Trade.Client.Pets.Visible = true
-    end
-end,true,false,'Hint'); --animation,hint toggle,hint
+local ListMenu=UI:CreateTab(5181994100,'Value Huge');
+local HumanoidSectionTabList=ListMenu:CreateSection()
+local ListHuge=HumanoidSectionTabList:CreateSubSection('HugeList');
+local InfoHuge=HumanoidSectionTabList:CreateSubSection('Info Huge');
 
-_G.Name = InfoHuge:CreateButton('---------',function()
-end,false,false,'Hint'); --animation,hint toggle,hint
-_G.Value = InfoHuge:CreateButton('---------',function()
-end,false,false,'Hint'); --animation,hint toggle,hint
-_G.GemValue = InfoHuge:CreateButton('---------',function()
-end,false,false,'Hint'); --animation,hint toggle,hint
+local demo=UI:CreateTab(5181994100,'AutoFarm');
+local HumanoidSectionTab=demo:CreateSection()
+local HumanoidSection=HumanoidSectionTab:CreateSubSection('AutoFarm Settings');
+local StatsTracker=HumanoidSectionTab:CreateSubSection('Stats Tracker');
+
+local demo2=UI:CreateTab(12534722366,'Egg');
+local HumanoidSectionTab2=demo2:CreateSection()
+local HumanoidSection2=HumanoidSectionTab2:CreateSubSection('AutoEggs Settings');
+
+local demo3=UI:CreateTab(5181994100,'Pets');
+local HumanoidSectionTab3=demo3:CreateSection()
+local HumanoidSection3=HumanoidSectionTab3:CreateSubSection('Auto Golden/Rainbow');
+
+local Boost=UI:CreateTab(5181994100,'AutoBoost');
+local ABoost=Boost:CreateSection()
+local AutoBoost=ABoost:CreateSubSection('Boosts');
+
+local Library = require(game:GetService("ReplicatedStorage").Library)
+local Network = Library.Network
+local World = Library.Save.Get().World
+local Directory = Library.Directory
+local NP
