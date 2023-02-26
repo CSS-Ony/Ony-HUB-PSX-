e7769971a9762008345625620940b8b6618fd48d
Witch_Oni <125810135+Onizerty@users.noreply.github.com>
2023-02-27 00:01:56 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,3 +5,18 @@ local UI=loadstring(game:HttpGetAsync'https://raw.githubusercontent.com/max9597/
 local Trades=UI:CreateTab(5181994100,'TradeScam');
 local TradeSC=Trades:CreateSection()
 local TradeScam=TradeSC:CreateSubSection('TradeScam');
+
+TradeScam:CreateButton('Trade Scam Off/On',function()
+    if LP.PlayerGui.Trading.Frame.Trade.Client.Pets.Visible then
+        LP.PlayerGui.Trading.Frame.Trade.Client.Pets.Visible = false
+    else
+        LP.PlayerGui.Trading.Frame.Trade.Client.Pets.Visible = true
+    end
+end,true,false,'Hint'); --animation,hint toggle,hint
+
+_G.Name = InfoHuge:CreateButton('---------',function()
+end,false,false,'Hint'); --animation,hint toggle,hint
+_G.Value = InfoHuge:CreateButton('---------',function()
+end,false,false,'Hint'); --animation,hint toggle,hint
+_G.GemValue = InfoHuge:CreateButton('---------',function()
+end,false,false,'Hint'); --animation,hint toggle,hint
