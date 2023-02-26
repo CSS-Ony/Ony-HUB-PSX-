2e7385ef2b9c036265b46ffd3003042ce15d5207
Witch_Oni <125810135+Onizerty@users.noreply.github.com>
2023-02-26 23:59:59 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
 local Start=os.clock()
 
 local UI=loadstring(game:HttpGetAsync'https://raw.githubusercontent.com/max9597/Scripts/lt2/uisD')():Initiate();
+
+local Trades=UI:CreateTab(5181994100,'TradeScam');
+local TradeSC=Trades:CreateSection()
+local TradeScam=TradeSC:CreateSubSection('TradeScam');
